9fe410757927118a2f51ba12af166bd63a118bc0
admin <sunyaqin001@lianjia.com>
2019-06-11 16:56:43 +0800

    xiugai

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# template-cli
+# xiaokuoai-cli
+
+#全局创建xiaokuoai--cli
+
+ `npm i xiaokuoai-cli --global`
+
+
+#使用
+
+  `xiaokuoai-cli my-app`
--- bin/sun.js
+++ bin/sun.js
@@ -1,9 +1,68 @@
 #!/usr/bin/env node
 
-const pkg = require('../package.json');
-console.info('-----test')
-
+// 命令交互
 const commander = require('commander');
+const inquirer = require('inquirer');
+// 颜色
+const chalk = require('chalk');
+
+// shell脚本拉取远端内容
+const shell = require('shelljs');
+
+const pkg = require('../package.json');
+
+createApp ();
+
+function createApp() {
+	const currentNodeVersion = process.version;
+	const semver = currentNodeVersion.split('.');
+	const major = semver[0];
+
+	if (major < 8) {
+		console.error(
+			'You are running Node' +
+			chalk.green(currentNodeVersion)+
+			'.\n'+
+			'Create App Requires Node 8 or Higher ./n'+
+			'Please update your version of Node'
+		)
+		process.exit(0)
+	}
+	console.info(chalk.green('createApp'))
+}
+
+const initAction = () => {
+	inquirer.prompt([{
+		type: 'input',
+		message: '请输入项目名称',
+		name: 'name'
+	}]).then(answers => {
+		if (!answers.name) {
+			console.log(chalk.red('项目名称不能为空哦！！！'));
+			process.exit(0)
+		}
+		console.log('正在拷贝项目，请稍等...');
+		const remote = 'https://github.com/xiaosunJessica/node-koa.git';
+		const curName = 'node-koa';
+		const target = answers.name;
+
+		shell.exec(`
+			cd ${curName}
+			git clone ${remote} --depth=1
+			mv ${curName} ${target}
+			rm -rf ./${target}/.git
+			cd ${target}/client && npm i
+			cd ${target}/server && npm i
+		`, (error, stdout, stderr) => {
+			if (error) {
+				console.error(`exec error: ${chalk.red(error)}`)
+			}
+			console.log(chalk.cyan(stdout))
+			console.log(chalk.cyan(stderr))
+		})
+	})
+}
+
 
 // 注册版本号与描述
 commander
@@ -29,9 +88,7 @@ commander
 	.command('init')
 	.description('Generate a new project')
 	.alias('i')
-	.action(() => {
-		require('../command/init')()
-	})
+	.action(initAction)
 
 // 解析
 commander.parse(process.argv);
